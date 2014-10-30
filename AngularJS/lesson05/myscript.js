76ab34a8d7e87f597bf40b0e275c7665cd74cc49
Kei <borgfrog@gmail.com>
2014-10-30 09:10:24 +0900

    Lesson 5
    Filter, LimitTo, OrderBy

---
+++ AngularJS/lesson05/myscript.js
@@ -0,0 +1,10 @@
+var mainCtrl = function($scope) {
+	$scope.users = [
+		{"name":"ai", "score":3.14},
+		{"name":"kai", "score":34.4},
+		{"name":"sai", "score":1.192},
+		{"name":"tai", "score":14.14},
+		{"name":"nai", "score":-0.14},
+		{"name":"hai", "score":123.4}
+	];
+}
